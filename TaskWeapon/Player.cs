53281816882207a5a899914b440e30f6f5665a28
S1monTheP1eMan <70568227+S1monTheP1eMan@users.noreply.github.com>
2021-07-20 18:14:23 +0300

    Added code

---
+++ TaskWeapon/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TaskWeapon
+{
+    class Player
+    {
+        public bool IsAlive { get; private set; }
+
+        private int _health;
+
+        public Player(int health)
+        {
+            if (health <= 0)
+                throw new ArgumentOutOfRangeException(nameof(health));
+
+            IsAlive = true;
+            _health = health;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            _health -= damage;
+
+            if (_health <= 0)
+                IsAlive = false;
+        }
+    }
+}
---
+++ TaskWeapon/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TaskWeapon
+{
+    class Weapon
+    {
+        private int _damage;
+        private int _bullets;
+
+        public Weapon(int damage, int bullets)
+        {
+            if (bullets < 0)
+                throw new ArgumentOutOfRangeException(nameof(bullets));
+
+            if (damage < 0)
+                throw new ArgumentOutOfRangeException(nameof(damage));
+
+            _damage = damage;
+            _bullets = bullets;
+        }
+
+        public void Fire(Player player)
+        {
+            if (_bullets <= 0)
+                throw new InvalidOperationException();
+
+            if (player.IsAlive)
+            {
+                player.TakeDamage(_damage);
+                _bullets -= 1;
+            }
+        }
+    }
+}
